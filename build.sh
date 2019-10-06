2bc91d6d2967d727dcb27e953c112f450267709f
MarioJim <mario.emilio.j@gmail.com>
2019-10-06 11:05:38 -0500

    Updated and shortened build.sh

--- build.sh
+++ build.sh
@@ -1,17 +1,13 @@
 #!/bin/sh
 
-PORTFOLIO=false
-RESUME=false
+COPY_FILES=false
 SHOW_STATUS=false
-# 0 = quiet, 1 = normal, 2 = verbose
-VERBOSITY=1
+VERBOSITY=1 # 0 = quiet, 1 = normal, 2 = verbose
 
 log() {
-    if ((VERBOSITY == 0))
-    then
+    if ((VERBOSITY == 0)); then
         "$@" &>/dev/null
-    elif ((VERBOSITY == 1))
-    then
+    elif ((VERBOSITY == 1)); then
         "$@" 1>/dev/null
     else
         "$@"
@@ -20,11 +16,10 @@ log() {
 
 print_help() {
     cat <<HELP
-Usage: $(basename $0) [OPTION]
-Script to compile and build my portfolio and/or my resume.
+Usage: ./$(basename $0) [options]
+Script to compile and build my portfolio and my resume.
 
-    -p, --portfolio     Build my portfolio
-    -r, --resume        Build my resume
+    -c, --copy          Copy files into the website repository          
     -s, --status        Show 'git status' on the website repository
     -v, --verbose       Verbose (stdout and stderr)
     -q, --quiet         Quiet (only progress)
@@ -34,15 +29,9 @@ HELP
 }
 
 parse() {
-    if (( $# == 0 )); then
-        echo "No arguments passed."
-        print_help
-        exit 0
-    fi
     while [[ "$1" == -* ]]; do
         case "$1" in
-            -p|--portfolio) PORTFOLIO=true;;
-            -r|--resume) RESUME=true;;
+            -c|--copy) COPY_FILES=true;;
             -s|--status) SHOW_STATUS=true;;
             -v|--verbose) VERBOSITY=2;;
             -q|--quiet) VERBOSITY=0;;
@@ -60,40 +49,34 @@ parse() {
     done
 }
 
-installDependencies() {
-    if [ ! -d "node_modules" ]; then
-        echo " ✔ Installing $1 dependencies"
-        log yarn install
+checkDependencies() {
+    echo " ✔ Checking dependencies"
+    if [ $(which lerna > /dev/null 2>&1) ]; then
+        echo "lerna not installed, run 'sudo npm i lerna -g'"
+        exit 1
+    fi
+    if [ $(which xelatex > /dev/null 2>&1) ]; then
+        echo "xelatex not installed, add package 'texlive'"
+        exit 1
     fi
 }
 
-buildData() {
-    pushd data > /dev/null
-    installDependencies "data"
-    if [ -d "lib" ]; then
-        echo " ✔ Cleaning up data folder"
-        rm -r lib
-    fi
-    echo " ✔ Compiling data to JavaScript"
-    log yarn build
-    popd > /dev/null
-}
-
-checkSymlink2Data() {
-    if [ ! -L "node_modules/data" ]; then
-        echo " ✔ Linking dependency to local data package"
-        rm -r node_modules/data
-        ln -s ../../data node_modules/data
-    fi
-}
-
-checkDestGitRepo() {
-    if [ ! -d "../../mariojim.github.io" ]; then
+copyToRepo() {
+    if [ ! -d "../mariojim.github.io" ]; then
         echo " ✔ Cloning server repository"
-        pushd ../../ > /dev/null
+        pushd ../ > /dev/null
         log git clone https://github.com/MarioJim/mariojim.github.io.git
         popd > /dev/null
     fi
+    echo " ✔ Clearing mariojim.github.io"
+    rm -r ../mariojim.github.io/*
+    echo " ✔ Moving files to mariojim.github.io"
+    mv portfolio/public/* ../mariojim.github.io
+    rmdir portfolio/public
+    pushd ../mariojim.github.io > /dev/null
+    log git restore LICENSE README.md
+    log git add .
+    popd > /dev/null
 }
 
 printWebsiteRepoStatus() {
@@ -104,67 +87,15 @@ printWebsiteRepoStatus() {
     popd > /dev/null
 }
 
-buildResume() {
-    pushd resume > /dev/null
-    installDependencies "resume"
-    checkSymlink2Data
-    if [ -d "build" ]; then
-        echo " ✔ Cleaning up build folder"
-        rm -r build
-    fi
-    mkdir -p build/sections
-    echo " ✔ Compiling data to LaTeX"
-    log yarn build
-    cp -r static/* build
-    echo " ✔ Compiling LaTeX to pdf"
-    pushd build > /dev/null
-    log xelatex resume.tex
-    popd > /dev/null
-    echo " ✔ Copying resume.pdf to portfolio"
-    cp build/resume.pdf ../portfolio/static/resume.pdf
-    checkDestGitRepo
-    if ! $PORTFOLIO; then
-        echo " ✔ Copying resume.pdf to webpage"
-        cp build/resume.pdf ../../mariojim.github.io/resume.pdf
-    fi
-    echo " ✔ Cleaning up resume folder"
-    rm -r build
-    popd > /dev/null
-}
-
-buildPortfolio() {
-    pushd portfolio > /dev/null
-    installDependencies "portfolio"
-    checkSymlink2Data
-    if [ -d "public" ]; then
-        echo " ✔ Cleaning up public folder"
-        rm -r public
-    fi
-    echo " ✔ Compiling to a webpage"
-    log yarn build
-    checkDestGitRepo
-    echo " ✔ Clearing mariojim.github.io"
-    rm -r ../../mariojim.github.io/*
-    echo " ✔ Moving files to mariojim.github.io"
-    mv public/* ../../mariojim.github.io
-    rmdir public
-    pushd ../../mariojim.github.io > /dev/null
-    log git restore LICENSE README.md
-    log git add .
-    popd > /dev/null
-    popd > /dev/null
-}
-
 main() {
     parse "$@"
-    if $PORTFOLIO || $RESUME; then
-        buildData
-    fi
-    if $RESUME; then
-        buildResume
-    fi
-    if $PORTFOLIO; then
-        buildPortfolio
+    checkDependencies
+    echo " ✔ Installing and symlinking dependecies"
+    log lerna bootstrap
+    echo " ✔ Building and compiling, please wait"
+    log lerna run build
+    if $COPY_FILES; then
+        copyToRepo
     fi
     if $SHOW_STATUS; then
         printWebsiteRepoStatus
