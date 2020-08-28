8763ac79efbe7a0cbc7ef8e33675b028fc64389d
MarioJim <mario.emilio.j@gmail.com>
2020-08-28 07:10:30 +0000

    Fixing GitHub actions #6

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Usage: ./$(basename $0) [options]
 # Script to compile my portfolio and resume
+# Mario Jiménez <mario.emilio.j@gmail.com>
 
 echo " ✔ Installing and symlinking dependecies"
 yarn install
@@ -11,3 +11,5 @@ echo " ✔ Compiling resume"
 yarn workspace resume build
 echo " ✔ Building portfolio"
 yarn workspace portfolio build
+echo " ✔ Moving resume"
+yarn workspace resume move
